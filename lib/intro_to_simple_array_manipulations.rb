c81f36afa18778613b5445455f7ae6abc4455a16
SANJAY K SAI <shachsan@hotmail.com>
2018-10-16 02:22:55 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,27 +1,39 @@
 def using_push(countries_in_western_africa, next_country)
   countries_in_western_africa.push(next_country)
-end 
+end
 
 def using_unshift(arr, string)
   arr.unshift(string)
 end
 
 def using_pop(arr)
-  arr.pop 
-end 
+  arr.pop
+end
 
 def pop_with_args(arr, n)
   arr.pop(n)
-end 
+end
 
 def using_shift(arr)
   arr.shift
-end 
+end
 
 def shift_with_args(arr, n)
   arr.shift(n)
-end 
+end
 
 def using_concat(arr1, arr2)
   return arr1 + arr2
-end 
+end
+
+def using_insert(arr, ele)
+  arr.insert(4, ele)
+end
+
+def using_uniq(arr)
+  arr.uniq
+end
+
+def using_flatten(arr)
+  arr.flatten
+end
